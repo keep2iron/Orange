f230b127c9796ca8d9dcd8ae1f8ae52810916768
keep2iron <497881309@qq.com>
2017-11-07 15:16:29 +0800

    添加sample。并且修改databinding相关的支持

---
+++ sample/src/main/java/io/github/keep2iron/finishadpter/MainActivity.java
@@ -0,0 +1,21 @@
+package io.github.keep2iron.finishadpter;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.keep2iron.finishadpter.databinding.ActivityMainBinding;
+
+/**
+ * @author keep2iron
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        RecyclerModule recyclerModule = new RecyclerModule(binding.recyclerView);
+        binding.setViewModule(recyclerModule);
+    }
+}
